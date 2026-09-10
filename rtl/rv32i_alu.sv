8bb3426bf142e9b738a449be1934d26a556d0eef
eugene1028csh <137296780+eugene1028csh@users.noreply.github.com>
2026-09-10 02:47:20 +0000

    alu rtl

--- rtl/rv32i_alu.sv
+++ rtl/rv32i_alu.sv
@@ -18,4 +18,21 @@ module rv32i_alu (
     output logic [31:0] result
 );
 
+//always_comb means this block describes a combinational logic
+always_comb begin
+    case(alu_op)
+        ALU_ADD: result = operand_a + operand_b;
+        ALU_SUB: result = operand_a - operand_b;
+        ALU_AND: result = operand_a & operand_b;
+        ALU_OR: result = operand_a | operand_b;
+        ALU_XOR: result = operand_a ^ operand_b;
+        ALU_SLL: result = operand_a << operand_b[4:0]; //But there’s one catch: for RV32, only the lower 5 bits of the shift amount matter.
+        ALU_SRL: result = operand_a >> operand_b[4:0]; //always fills from the left with 0
+        ALU_SRA: result = $signed(operand_a) >>> operand_b[4:0]; //fills from the left with the old sign bit
+        ALU_SLT: result = $signed(operand_a) < $signed(operand_b);
+        ALU_SLTU: result = operand_a < operand_b;
+        default: result = 32'b0;
+    endcase
+end
+
 endmodule
