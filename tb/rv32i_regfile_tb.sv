61878aa1115b4c0f259163fac89607af33975487
eugene1028csh <137296780+eugene1028csh@users.noreply.github.com>
2026-09-10 08:11:50 +0000

    regfile tb!!

--- tb/rv32i_regfile_tb.sv
+++ tb/rv32i_regfile_tb.sv
@@ -29,6 +29,8 @@ module rv32i_regfile_tb;
     end
 
     initial begin
+        rs1_addr = 5'd0;
+        rs2_addr = 5'd0;
         write_en = 1;
         wr_addr = 5'd4;
         wr_data = 32'd123;
@@ -41,6 +43,91 @@ module rv32i_regfile_tb;
         assert (rs1_data == 32'd123)
             else $error("rs1_data wrong");
 
+        //test x0
+        rs1_addr = 5'd6;
+        rs2_addr = 5'd7;
+        write_en = 1;
+        wr_addr = 5'd0;
+        wr_data = 32'd67;
+
+        @(posedge clk);
+        rs1_addr = 5'd0;
+
+        #1;
+
+        assert (rs1_data == 32'd0)
+            else $error("rs1_data wrong");
+
+        //2 registers
+        //write x6 = 67
+        write_en = 1;
+        wr_addr = 5'd6;
+        wr_data = 32'd67;
+        @(posedge clk);
+        #1;
+
+        //write x7 = 47
+        write_en =1;
+        wr_addr = 5'd7;
+        wr_data = 32'd47;
+        @(posedge clk);
+        #1;
+
+        //read both simulataneously
+        write_en = 0;
+        rs1_addr = 5'd6;
+        rs2_addr = 5'd7;
+
+        #1;
+
+        assert (rs1_data == 32'd67)
+            else $error("rs1_data wrong");
+        assert (rs2_data == 32'd47)
+            else $error("rs2_data wrong");
+
+        //write_en is 0
+        write_en = 0;
+        wr_addr = 5'd6;
+        wr_data = 32'd88;
+
+        @(posedge clk);
+        #1;
+
+        rs1_addr = 5'd6;
+        
+        #1;
+
+        assert (rs1_data == 32'd67)
+            else $error("rs1_data wrong");
+        
+        //overwrite register test
+        write_en = 1;
+        wr_addr = 5'd6;
+        wr_data = 32'd90;
+
+        @(posedge clk);
+        rs1_addr = 5'd6;
+
+        #1;
+
+        assert (rs1_data == 32'd90)
+            else $error("rs1_data wrong");
+
+        //read from x0
+        write_en = 0;
+        rs1_addr = 5'd0;
+        rs2_addr = 5'd0;
+
+        #1;
+
+        assert (rs1_data == 32'd0)
+            else $error("x0 read port 1 failed");
+
+        assert (rs2_data == 32'd0)
+            else $error("x0 read port 2 failed");
+
+    $display("Register file write/read test passed!");
+    $finish;
     end
 
 endmodule
